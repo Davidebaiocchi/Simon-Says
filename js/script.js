81524a74ea8ef59316c31ac628fd1bfe0737b8c9
davide <davide.baiocchi97@gmail.com>
2021-06-11 16:06:32 +0200

    push

--- js/script.js
+++ js/script.js
@@ -6,9 +6,12 @@
 // il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
 
-// creo un alert con i 5 numeri random
+// creo due array 
 var randomNumbers = [];
+var userNumbers = [];
 
+
+// creo un alert con i 5 numeri random
 while (randomNumbers.length < 5) {
     var numeroCasuale = Math.floor(Math.random() * 100 ) +1;
 
@@ -19,12 +22,32 @@ while (randomNumbers.length < 5) {
 alert(randomNumbers);
 
 
+// timer 30 secondi
+var timeleft = 2;
+var downloadTimer = setInterval(function(){
+  document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
+  timeleft -= 1;
+  if(timeleft <= 0){
+    clearInterval();
+    document.getElementById("countdown").innerHTML = "Finished"
+  }
+}, 1000);
 
 
+ // Chiedo i 5 numeri all utente
 
-
-
-
-
-
-// --------------> Function
+for(i = 0; i < 5; i++) {
+var usNumber = parseInt(prompt('Inserisci un numero che ti ricordi'));
+if (!randomNumbers.includes(usNumber)) {
+    userNumbers.push(usNumber);
+} else {
+    alert('Numero precendentemente utilizzato!');
+}
+}    
+    
+// Confronto i numeri dell'utente e quelli random, quelli che coincidono li stampo. Segno il punteggio.
+for(i = 0; i < 5; i++) {
+    if(userNumber[i] == randomNumbers[i]) {
+    document.getElementById("countdown").innerHTML = userNumber[i] + 'Hai ottenuto ' + userNumber[i] + 'Punti';
+    }
+}
